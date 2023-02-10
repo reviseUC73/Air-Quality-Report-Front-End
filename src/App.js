8f3c9e35bf6f1eb97c391554a8ea442980d1992d
Setthanan Thongpanchang <87871212+reviseUC73@users.noreply.github.com>
2023-02-10 12:14:27 +0700

    create init panal done

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-import ResponsiveAppBar from './components/Nav_ber';
-import Panel from './components/Panel';
+// import logo from './logo.svg';
+import "./App.css";
+import ResponsiveAppBar from "./components/Nav_ber";
+import Panel from "./components/Panel";
 
 function App() {
   return (
     <div className="App">
-     
-      
-      <ResponsiveAppBar/>
-      <Panel/>
+      <ResponsiveAppBar />
+      <Panel weather="" number="" typeofair="" />
     </div>
   );
 }
--- src/components/Panel.js
+++ src/components/Panel.js
@@ -1,10 +1,23 @@
 import React from "react";
 import "./Panal.css";
-export default function Panel() {
+export default function Panel(props) {
+  const weather = props.weather || "default";
+  const number = props.number || "--";
+  const typeofair = props.TypeOfAir || "Type of Air";
+
   return (
     <div>
       <div className="container">
-        <div className="p_title">Weather</div>
+        {/* <div className="p_title">Weather</div> */}
+        <img
+          class="icon_po"
+          src="https://cdn3d.iconscout.com/3d/premium/thumb/raining-at-night-3321909-2775272.png"
+          alt="1"
+          border="0"
+        />
+        <div className="name_po">{weather}</div>
+        <div className="number">{number}°C</div>
+        <div id="type_measure"> {typeofair} </div>
       </div>
     </div>
   );
