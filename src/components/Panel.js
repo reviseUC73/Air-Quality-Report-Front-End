ce92befc62a16801ce9960d7bdca4e2c66f65b62
Setthanan Thongpanchang <87871212+reviseUC73@users.noreply.github.com>
2023-02-10 10:12:45 +0700

    first init

---
+++ src/components/Panel.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Panal.css";
+export default function Panel() {
+  return (
+    <div>
+      <div className="container">
+        <div className="p_title">Weather</div>
+      </div>
+    </div>
+  );
+}
